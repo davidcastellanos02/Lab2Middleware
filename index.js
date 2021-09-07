3b66526832c3527972e798adbcb0eab1f0e2c82f
Adriana Castellanos A Jesús por María <davidsantiagocastellanos@gmail.com>
2021-09-07 00:34:32 -0500

    transform image

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+
+var current = 0
+
+var instances = [
+    "http://localhost:3021"
+    //,"http://localhost:3022"
+]
+
+app.get('/', (req,res) => {
+    const server  = instances[current]
+    //current > instances.length ? current = 0 : current++
+    res.redirect(server)
+})
+
+app.listen(3020)
